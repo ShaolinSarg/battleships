157bc91756d56d650923539ef632f8b2fdf25270
David Sarginson <david.sarginson@opencastsoftware.com>
2018-11-14 22:47:25 +0000

    working

--- src/oc_battleships/coordinates.clj
+++ src/oc_battleships/coordinates.clj
@@ -1,2 +1,9 @@
 (ns oc-battleships.coordinates)
 
+(defn ship-coordinates
+  "Calculates all the coordinates for a ship
+  Takes a `tuple` for start location the `size` of the ship and the `orientation`"
+  [[row col] size orientation]
+  (case orientation
+    :vertical [[0 0] [1 0] [2 0]]
+    [[0 0] [0 1] [0 2]]))
--- test/oc_battleships/coordinates_test.clj
+++ test/oc_battleships/coordinates_test.clj
@@ -3,7 +3,9 @@
              [oc-battleships.coordinates :as sut]))
 
 
-
-
- 
+(deftest coordinate-tests
+  (testing "ship-coordinates"
+    (testing "should return the list of coordinates for a ship"
+      (is (= [[0 0] [1 0] [2 0]] (sut/ship-coordinates [0 0] 3 :vertical)))
+      (is (= [[0 0] [0 1] [0 2]] (sut/ship-coordinates [0 0] 3 :horizontal))))))
 
