1522bc3f698f5b7723f89c989473be28632cbdbc
David Sarginson <david.sarginson@opencastsoftware.com>
2018-11-14 22:55:54 +0000

    working

--- src/oc_battleships/coordinates.clj
+++ src/oc_battleships/coordinates.clj
@@ -5,5 +5,5 @@
   Takes a `tuple` for start location the `size` of the ship and the `orientation`"
   [[row col] size orientation]
   (case orientation
-    :vertical [[0 0] [1 0] [2 0]]
+    :vertical (for [z (range size)] [z 0])
     [[0 0] [0 1] [0 2]]))
--- test/oc_battleships/coordinates_test.clj
+++ test/oc_battleships/coordinates_test.clj
@@ -7,5 +7,6 @@
   (testing "ship-coordinates"
     (testing "should return the list of coordinates for a ship"
       (is (= [[0 0] [1 0] [2 0]] (sut/ship-coordinates [0 0] 3 :vertical)))
+      (is (= [[0 0] [1 0]] (sut/ship-coordinates [0 0] 2 :vertical)))
       (is (= [[0 0] [0 1] [0 2]] (sut/ship-coordinates [0 0] 3 :horizontal))))))
 
